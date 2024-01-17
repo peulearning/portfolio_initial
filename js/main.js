9532d61b8289f0bbef9440e607d6a1a37e90544a
Pedro <peuhenry2018@gmail.com>
2024-01-17 18:33:46 -0300

    Fixing Concepts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 // I will use it to apply some future things;
+/*
+document.addEventListener('DOMContentLoaded', function () {
+  const toggle = document.getElementById('toggle');
+
+  toggle.addEventListener('change', function () {
+    const navLinks = document.querySelector('.nav-links');
+
+    if (this.checked) {
+      navLinks.style.display = 'flex';
+    } else {
+      navLinks.style.display = 'none';
+    }
+  });
+});
+*/
